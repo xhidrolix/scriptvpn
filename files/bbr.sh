b41c4d3d39c49617ce004ae7a3ce2fd781648711
VnzVPN <69085043+xhidrolix@users.noreply.github.com>
2024-03-30 22:57:23 +0700

    Update bbr.sh

--- files/bbr.sh
+++ files/bbr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Edition : Stable Edition V3.0
-# Auther  : Tomket
+# Auther  : VnzVPN
 # (C) Copyright 2023
 # =========================================
 
@@ -18,7 +18,7 @@ clear
 #	 menu
 #else
 
-echo -e "Installing TCP BBR TOMKET"
+echo -e "Installing TCP BBR Vnz"
 echo -e "Please Wait BBR Installation Will Starting . . ."
 sleep 5
 clear
